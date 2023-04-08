0d43c16d3a630b31bb8925b25dd98880b15ed5f9
giffone <galimzhanov.faizulla@gmail.com>
2023-04-08 13:09:21 +0600

    add: bot cli

--- bot_cli/cmd/botcli/main.go
+++ bot_cli/cmd/botcli/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"tgbot/internal/bot"
+	"tgbot/internal/config"
+)
 
 func main() {
 	var token string
 	flag.StringVar(&token, "token", "", "telegram token")
 	flag.Parse()
+
+	// configuration
+	conf := config.NewBotConf(token)
+
+	// grpc cli
+
+
+	// telegram cli
+	b := bot.NewBot(conf)
 }
--- bot_cli/internal/bot/bot.go
+++ bot_cli/internal/bot/bot.go
@@ -1,1 +1,46 @@
 package bot
+
+import (
+	"log"
+	"tgbot/internal/config"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"google.golang.org/grpc"
+)
+
+type BotCli struct {
+	Bot     *tgbotapi.BotAPI
+	Updates tgbotapi.UpdatesChannel
+	GrpcCli *grpc.ClientConn
+}
+
+func NewBot(conf *config.BotConf) *BotCli {
+	bot, err := tgbotapi.NewBotAPI(conf.Token)
+	if err != nil {
+		log.Fatalf("new bot: %s", err.Error())
+	}
+	bot.Debug = conf.Debug
+
+	u := tgbotapi.NewUpdate(conf.Offset)
+	u.Timeout = conf.TimeOut
+
+	log.Printf("authorized as %s", bot.Self.UserName)
+
+	return &BotCli{
+		Bot:     bot,
+		Updates: bot.GetUpdatesChan(u),
+	}
+}
+
+func (b *BotCli) Listener() {
+	for update := range b.Updates {
+		if update.Message != nil {
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			b.Bot.Send(msg)
+		}
+	}
+}
