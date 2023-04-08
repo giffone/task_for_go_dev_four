0fe66ae93e238750f74fb57f2192c7261432fe4b
giffone <galimzhanov.faizulla@gmail.com>
2023-04-08 18:07:03 +0600

    add: grpc

--- bot_cli/cmd/botcli/main.go
+++ bot_cli/cmd/botcli/main.go
@@ -2,21 +2,29 @@ package main
 
 import (
 	"flag"
+	"log"
 	"tgbot/internal/bot"
 	"tgbot/internal/config"
+	"tgbot/internal/grpc_api"
 )
 
 func main() {
-	var token string
+	var addr, token string
 	flag.StringVar(&token, "token", "", "telegram token")
+	flag.StringVar(&addr, "addr", "localhost:50051", "the address to connect to")
 	flag.Parse()
+
+	// grpc cli
+	grpcCli, err := grpc_api.NewGrpcClient(addr)
+	if err != nil {
+		log.Fatalf("grpc client: %v", err)
+	}
+	defer grpcCli.Close()
 
 	// configuration
 	conf := config.NewBotConf(token)
 
-	// grpc cli
-
-
 	// telegram cli
-	b := bot.NewBot(conf)
+	b := bot.NewBot(conf, grpcCli)
+	b.Listener()
 }
--- bot_cli/internal/bot/bot.go
+++ bot_cli/internal/bot/bot.go
@@ -3,18 +3,18 @@ package bot
 import (
 	"log"
 	"tgbot/internal/config"
+	"tgbot/internal/grpc_api"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"google.golang.org/grpc"
 )
 
 type BotCli struct {
-	Bot     *tgbotapi.BotAPI
-	Updates tgbotapi.UpdatesChannel
-	GrpcCli *grpc.ClientConn
+	bot     *tgbotapi.BotAPI
+	updates tgbotapi.UpdatesChannel
+	grpcCli *grpc_api.Client
 }
 
-func NewBot(conf *config.BotConf) *BotCli {
+func NewBot(conf *config.BotConf, grpcCli *grpc_api.Client) *BotCli {
 	bot, err := tgbotapi.NewBotAPI(conf.Token)
 	if err != nil {
 		log.Fatalf("new bot: %s", err.Error())
@@ -27,20 +27,21 @@ func NewBot(conf *config.BotConf) *BotCli {
 	log.Printf("authorized as %s", bot.Self.UserName)
 
 	return &BotCli{
-		Bot:     bot,
-		Updates: bot.GetUpdatesChan(u),
+		bot:     bot,
+		updates: bot.GetUpdatesChan(u),
+		grpcCli: grpcCli,
 	}
 }
 
 func (b *BotCli) Listener() {
-	for update := range b.Updates {
+	for update := range b.updates {
 		if update.Message != nil {
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 			msg.ReplyToMessageID = update.Message.MessageID
 
-			b.Bot.Send(msg)
+			b.bot.Send(msg)
 		}
 	}
 }
